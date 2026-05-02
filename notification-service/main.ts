0e29d6db1230f06a87ae2a99e3a315eaa671b28e
Sadoune <Sadounefadjre321@gmail.com>
2026-05-02 18:37:41 +0100

    Enhance CORS settings in main.ts
    Updated CORS configuration to specify allowed origins, methods, and headers.

--- notification-service/main.ts
+++ notification-service/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   const port = process.env.PORT || 3002;
   await app.listen(port);
