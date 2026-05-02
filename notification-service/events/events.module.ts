e8d41be45558be8d83b520ccf142d51935bf427e
Sadoune <Sadounefadjre321@gmail.com>
2026-05-02 18:32:43 +0100

    Add MulterModule and StorageListener to EventsModule

--- notification-service/events/events.module.ts
+++ notification-service/events/events.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+
 import { ReservationListener } from './reservation.listener';
+import { StorageListener } from './storage.listener';
+import { StorageController } from './storage.controller';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [HttpModule, ScheduleModule.forRoot(), NotificationModule],
-  providers: [ReservationListener],
+  imports: [
+    HttpModule,
+    ScheduleModule.forRoot(),
+    NotificationModule,
+   
+    MulterModule.register({ storage: memoryStorage() }),
+  ],
+  controllers: [StorageController],
+  providers: [ReservationListener, StorageListener],
 })
 export class EventsModule {}
